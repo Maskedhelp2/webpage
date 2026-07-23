eaef08fa7d04c5af2140c198e6ba1be817b8622b
Maskedhelp2 <karthik31012007@outlook.com>
2026-07-23 09:49:30 +0530

    standalone output for opennext

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.0.111'],
-  output: 'export',
+  output: 'standalone',
 };
 
 export default nextConfig;
